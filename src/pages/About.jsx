8309e519d5f0a8209d6f58ada6d0619e1beb19e7
feinonly1 <feinonly@hotmail.com>
2025-07-27 16:48:47 +0300

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,36 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-const team = [
-	{
-		name: "Tuncay (feinonly)",
-		role: "Kurucu",
-		desc: "Topluluğun vizyonunu belirler, yenilikçi projeleri yönetir.",
-		avatar:
-			"https://cdn.discordapp.com/avatars/676793498640580608/e942a1beadb39c9921074f519224f57a?size=1024",
-	},
-  {
-		name: "Yamaç (asuwup)",
-		role: "Kurucu Ortak & Proje Yöneticisi",
-		desc: "Topluluğun vizyonunu belirler, yenilikçi projeleri yönetir.",
-		avatar:
-			"https://cdn.discordapp.com/avatars/458611579798421505/abf65e06958f87969ab1c226405146df?size=1024",
-	},
-  {
-		name: "Can (c4nko)",
-		role: "Ceo & Proje Yöneticisi",
-		desc: "Topluluğun vizyonunu belirler, yenilikçi projeleri yönetir.",
-		avatar:
-			"https://cdn.discordapp.com/avatars/770260411990671386/2f753e49b1e26571ea29aee70f355035?size=1024",
-	},
-  	{
-		name: "Halil (halilfirarda)",
-		role: "Üst Yönetim Kurulu (ÜYK)",
-		desc: "Topluluğun vizyonunu belirler, yenilikçi projeleri yönetir.",
-		avatar:
-			"https://cdn.discordapp.com/avatars/1103659555793141800/51bd29842c3f30eb13e56ad3d281baa9?size=1024",
-	},
-];
-
 function About() {
 	const [animate, setAnimate] = useState(false);
 
@@ -79,30 +48,6 @@ function About() {
 						Güvenli, saygılı ve eğlenceli bir ortam oluşturmak.
 					</li>
 				</ul>
-			</div>
-			<div
-				className={`about-section anim-fade-in-up${
-					animate ? " anim-in" : ""
-				}`}
-				style={{ marginTop: "2.5rem" }}
-			>
-				<h3>Ekibimiz</h3>
-				<div className="about-team">
-					{team.map((member, i) => (
-						<div className="about-team-card" key={i}>
-							<img
-								src={member.avatar}
-								alt={member.name}
-								className="about-team-avatar"
-							/>
-							<div>
-								<div className="about-team-name">{member.name}</div>
-								<div className="about-team-role">{member.role}</div>
-								<div className="about-team-desc">{member.desc}</div>
-							</div>
-						</div>
-					))}
-				</div>
 			</div>
 			<div
 				className={`about-section anim-fade-in-up${
